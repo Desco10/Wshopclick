b748746dbfaa82e6adcb02c54cd4d3bc140440d2
Desco10 <daniel1041esk.arb@gmail.com>
2025-06-29 17:41:10 -0500

    revisandolo

--- script.js
+++ script.js
@@ -1,50 +1,29 @@
+
 const productos = [
-  { nombre: "Advil max", precio: "$10.000", imagen: "https://i.ibb.co/pBjNS89H/Advilmax.jpg" },
-  { nombre: "Producto 2", precio: "$12.000", imagen: "https://i.ibb.co/GZVSRmJ/Advilgripa.jpg" },
-  { nombre: "Producto 3", precio: "$9.000", imagen: "https://i.ibb.co/6qxqk5y/advilgripamax.jpg" },
+  { nombre: "Advil Max", precio: "$10.000", imagen: "img/advilmax.jpeg" },
+  { nombre: "Bonfiest", precio: "$12.000", imagen: "img/bonfiest.png" },
+  { nombre: "Sevedol Extra", precio: "$9.000", imagen: "img/sevedol_estra.jpge" },
+  // más productos aquí...
 ];
 
-const productosPorPagina = 12;
-let paginaActual = 1;
+
 
 function mostrarProductos() {
   const contenedor = document.getElementById("productos");
   contenedor.innerHTML = "";
 
-  const inicio = (paginaActual - 1) * productosPorPagina;
-  const fin = inicio + productosPorPagina;
-  const productosPagina = productos.slice(inicio, fin);
-
-  productosPagina.forEach(prod => {
+  productos.forEach(prod => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
       <img src="${prod.imagen}" alt="${prod.nombre}">
       <h3>${prod.nombre}</h3>
       <p>${prod.precio}</p>
-      <a href="https://wa.me/573143416441?text=Hola quiero comprar: ${prod.nombre}%0AImagen: ${prod.imagen}" 
+      <a href="https://wa.me/573143416441?text=Hola, quiero este producto: ${encodeURIComponent(prod.nombre)}%0AValor: ${encodeURIComponent(prod.precio)}%0AImagen: ${location.origin}/${prod.imagen}"
          target="_blank" class="btn-wsp">WhatsApp</a>
     `;
     contenedor.appendChild(card);
   });
-
-  document.getElementById("paginaActual").textContent = `Página ${paginaActual}`;
-}
-
-function siguientePagina() {
-  const totalPaginas = Math.ceil(productos.length / productosPorPagina);
-  if (paginaActual < totalPaginas) {
-    paginaActual++;
-    mostrarProductos();
-  }
-}
-
-function anteriorPagina() {
-  if (paginaActual > 1) {
-    paginaActual--;
-    mostrarProductos();
-  }
 }
 
 mostrarProductos();
-
